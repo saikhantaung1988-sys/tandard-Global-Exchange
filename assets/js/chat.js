81e1b79177840d0efdde73259dc951de9a55169e
Student Deploever <studentdeploever@gmail.com>
2026-02-12 07:24:44 +0630

    update js

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,16 +1,4 @@
-// ၁။ Supabase ချိတ်ဆက်မှု (New Project Credentials)
 
-const supabaseUrl = 'https://qonmwtjznyrfenikqffk.supabase.co';
-
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFvbm13dGp6bnlyZmVuaWtxZmZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3NDQ3NzEsImV4cCI6MjA4NjMyMDc3MX0.OgTzG55xr_zkK8pIRp_S4lPG4eTIHunqz5PP_EPEFrs';
-
-
-
-const db = supabase.createClient(supabaseUrl, supabaseKey);
-
-
-
-console.log("✅ New Connection Established via core.js");
 
 // ၄။ SEND MESSAGE (User ဘက်က စာပို့ခြင်း - အရင်ပေးထားတဲ့ Optimistic UI)
 async function sendMessage() {
